efd68e0e46bc991bd1233b67d207e9776d7d9f12
Yevhen Vasyliev <ye.vasyliev@gmail.com>
2021-09-03 12:46:38 +0300

    fixed utils.resolveScreenName

--- src/main/java/api/longpoll/bots/methods/impl/utils/ResolveScreenName.java
+++ src/main/java/api/longpoll/bots/methods/impl/utils/ResolveScreenName.java
@@ -24,7 +24,7 @@ public class ResolveScreenName extends AuthorizedVkApiMethod<ResolveScreenName.R
 
     @Override
     protected Class<Response> getResponseType() {
-        return null;
+        return Response.class;
     }
 
     public ResolveScreenName setScreenName(String screenName) {
