d94e5a5d519b76f86e472b005e6162a69462d8b0
Yevhen Vasyliev <ye.vasyliev@gmail.com>
2022-07-15 23:03:17 +0300

    Removed static fields from GroupsGetMemberResponseDeserializer

--- src/main/java/api/longpoll/bots/adapters/deserializers/GroupsGetMemberResponseDeserializer.java
+++ src/main/java/api/longpoll/bots/adapters/deserializers/GroupsGetMemberResponseDeserializer.java
@@ -17,21 +17,19 @@ import java.util.List;
  * Deserializes JSON object to {@link GetMembers.ResponseBody}.
  */
 public class GroupsGetMemberResponseDeserializer implements JsonDeserializer<GetMembers.ResponseBody> {
-    private static final Type INTEGER_LIST = new TypeToken<List<Integer>>() {}.getType();
-    private static final Type ITEM_LIST = new TypeToken<List<GetMembers.ResponseBody.Item>>() {}.getType();
-
     @Override
     public GetMembers.ResponseBody deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
         JsonObject jsonResponse = jsonElement.getAsJsonObject().getAsJsonObject("response");
         JsonArray jsonItems = jsonResponse.getAsJsonArray("items");
+
 
         VkList<Object> vkList = new VkList<>();
         vkList.setCount(jsonResponse.get("count").getAsInt());
         vkList.setItems(jsonDeserializationContext.deserialize(
                 jsonItems,
                 jsonItems.size() == 0 || jsonItems.get(0).isJsonPrimitive()
-                        ? INTEGER_LIST
-                        : ITEM_LIST
+                        ? TypeToken.getParameterized(List.class, Integer.class).getType()
+                        : TypeToken.getParameterized(List.class, GetMembers.ResponseBody.Item.class).getType()
         ));
 
         GetMembers.ResponseBody response = new GetMembers.ResponseBody();
